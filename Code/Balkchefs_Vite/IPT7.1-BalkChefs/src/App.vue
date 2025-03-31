a7db84b5bcfc04729a171eac065818cc85920ee1
Erion Jasiqi <erion_jasiqi@sluz.ch>
2025-03-31 14:36:29 +0200

    started the vite code

--- Code/Balkchefs_Vite/IPT7.1-BalkChefs/src/App.vue
+++ Code/Balkchefs_Vite/IPT7.1-BalkChefs/src/App.vue
@@ -1,23 +1,14 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
+import HomeView from './views/HomeView.vue'
 </script>
 
 <template>
-  <div> 
-    <div>
-      <a href="https://vitejs.dev" target="_blank">
-        <img src="/vite.svg" class="logo" alt="Vite logo" />
-      </a>
-      <a href="https://vuejs.org/" target="_blank">
-        <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-      </a>
-    </div>
-    <HelloWorld msg="Vite + Vue" />
-  </div>
+  <HomeView/>
 </template>
 
 <style scoped>
-.logo {
+.logo { 
   height: 6em;
   padding: 1.5em;
   will-change: filter;
---
+++ Code/Balkchefs_Vite/IPT7.1-BalkChefs/src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<script>
+
+</script>
+<template>
+<html>
+
+<div class="p-6 bg-red-600">asdfipasf</div>
+
+<h1 class="text-3xl">Hello</h1>
+
+</html>
+</template>
+
+<style>
+</style>
