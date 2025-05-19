8d929ba2e656042c5538626ec731a91034bad0db
Erion Jasiqi <erion_jasiqi@sluz.ch>
2025-05-19 14:03:25 +0200

    fixed issue with the homeview router

--- Code/Balkchefs_Vite/IPT7.1-BalkChefs/src/router/index.js
+++ Code/Balkchefs_Vite/IPT7.1-BalkChefs/src/router/index.js
@@ -1,30 +1,33 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from '../views/Home.vue';
+import Login from '../views/Login.vue';
 import Recipe from '../views/Recipe.vue';
 
 const routes = [
-  { path: '/', component: Home },
-  { path: '/recipe/:id', component: Recipe }
+  {
+    path: '/',
+    name: 'home',
+    component: Home
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: Login
+  },
+  {
+    path: '/recipe/:id',
+    name: 'recipe',
+    component: Recipe
+  }
 ];
 
-
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
-    routes: [
-      {
-        path: '/',
-        name: 'home',
-        component: HomeView
-      },
-      {
-        path: '/login',
-        name: 'login',
-        component: LoginView
-      },
-    ]
-  })
-  
-  export default router
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes
+});
 
+export default router;
 
-  
+ 
+ 
+ 
